f7f1e81e33b37db066a03f217caa618cbbe07f91
Jeané Christine Thomas <christine.jeanethomas22@gmail.com>
2024-11-03 14:54:01 +0200

    Designing the layout and format of the form.

--- PRG282-Project/PRG282-Project/Form1.cs
+++ PRG282-Project/PRG282-Project/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace PRG282_Project
 {
-    public partial class Form1 : Form
+    public partial class frmStudent : Form
     {
-        public Form1()
+        public frmStudent()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
